18237113a27059ad78e02315d03a5878a0f35126
Meriem1551 <meriem200409@gmail.com>
2023-10-10 06:40:45 +0200

    Adding funcs for the input

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include "main.h"
+#include "../Search-algorithms/main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+    unsigned int l1, l2;
+    l1 = get_input("Enter the first array's size: ");
+    l2 = get_input("Enter the second array's size: ");
+    char *c1 = get_string_array("Enter the string as an array", l1);
+    char *c2 = get_string_array("Enter the string as an array", l2);
+    int res = is_anagram(c1, c2, l1, l2);
 }
--- main.h
+++ main.h
@@ -1,1 +1,2 @@
 int is_anagram(char[], char[], int, int);
+char *get_string_array(char *s, int);
