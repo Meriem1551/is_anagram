cb8f89bcbe1d86b8bc2f2db6418758afdc971f26
Meriem1551 <meriem200409@gmail.com>
2023-10-10 06:54:22 +0200

    Defining the func

--- is_anagram.c
+++ is_anagram.c
@@ -1,4 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 int is_anagram(char t[], char m[], unsigned int len1, unsigned int len2)
 {
+    unsigned int i, j, count = 0;
+    if (len1 == len2)
+    {
+        for (i = 0; i < len1; i++)
+        {
+            for (j = 0; j < len2; j++)
+            {
+                if (t[i] == m[j])
+                {
+                    count++;
+                }
+            }
+        }
+        if (count == len1)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        return 0;
+    }
     // code
 }
